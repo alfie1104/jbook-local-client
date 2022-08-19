b23cb9e416246de1aa74d27cbfb2660bdb7d022a
peter_alfie <alfie1104@gmail.com>
2022-08-19 13:41:15 +0900

    feat(index): add basic form elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
-import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
 const App = () => {
-  return <h1>hi</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}>
+        {input}
+      </textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
-ReactDom.render(<App />, document.querySelector("#root"));
+const root = createRoot(document.querySelector("#root")!);
+root.render(<App />);
