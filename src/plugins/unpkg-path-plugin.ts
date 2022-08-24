85f221a50e91304737573a2e35b90f06028d0f99
peter_alfie <alfie1104@gmail.com>
2022-08-24 13:27:19 +0900

    feat(unpkg-path-plugin): issues with Multi-file packages and solving by using url constructor

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -11,16 +11,17 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: "a" };
         }
 
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
+          };
+        }
+
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-        // else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
