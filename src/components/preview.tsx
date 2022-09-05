5a93672b5f2cab8f90813995a38540a536882e51
peter_alfie <alfie1104@gmail.com>
2022-09-05 13:36:19 +0900

    feat(index, preview): refactoring out a preview component

---
+++ src/components/preview.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from "react";
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try{
+              eval(event.data);
+            }catch(err){
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color:red;"><h4>Rutime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          },false);
+        </script>
+      </body>
+    </html>
+  `;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = html;
+
+    /*
+    postMessage 사용법
+    - 문법 : targetWindow.postMessage(message, targetOrigin, [transfer]);
+    targetWindow : 메세지를 전달 받을 window의 참조
+    message : 다른 widnow에 보내질 데이터
+    targetOrigin : targetWindow의 origin을 지정. '*'은 별도로 지정하지 않음을 나타냄. 문자열 혹은 URI를 지정해야함
+                  이벤트를 전송하려 할 때 targetWindow의 스키마, 호스트이름, 포트가 targetOrigin과 맞지 않다면 이벤트는 전송되지 않음
+
+    */
+    iframe.current.contentWindow.postMessage(code, "*");
+  }, [code]);
+
+  return (
+    <iframe
+      title="preview"
+      ref={iframe}
+      srcDoc={html}
+      sandbox="allow-scripts"
+    />
+  );
+};
+export default Preview;
