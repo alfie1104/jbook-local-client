45ccc463f097dd8d040229c6fbd319587995aaeb
peter_alfie <alfie1104@gmail.com>
2022-09-21 08:53:12 +0900

    feat(cellsreducer): removing a Cell

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell";
@@ -18,33 +19,21 @@ const initialState: CellsState = {
   order: [],
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content,
-          },
-        },
-      };
+      state.data[id].content = content;
+      return;
     case ActionType.DELETE_CELL:
-      return state;
+      return;
     case ActionType.MOVE_CELL:
-      return state;
+      return;
     case ActionType.INSERT_CELL_BEFORE:
-      return state;
+      return;
     default:
-      return state;
+      return;
   }
-};
+});
 
 export default reducer;
