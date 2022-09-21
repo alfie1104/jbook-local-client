d162b539bc5fd66af8803c5d184e50be65b4eab1
peter_alfie <alfie1104@gmail.com>
2022-09-22 08:46:05 +0900

    feat(cell-list, use-typed-selector): creating cell list components and Typed Selector

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
-//import CodeCell from "./components/code-cell";
 import { Provider } from "react-redux";
 import { store } from "./state";
-import TextEditor from "./components/text-editor";
+import CellList from "./components/cell-list";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        {/* <CodeCell /> */}
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -68,7 +68,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
     default:
       return state;
   }
-});
+}, initialState);
 
 const randomId = () => {
   //convert numbers to 0-9 & A-Z
