edff290b19e784353c113fa1d1d86bda20fcd88f
peter_alfie <alfie1104@gmail.com>
2022-09-29 08:17:34 +0900

    feat(bundlesreducer, reducers/index): bundles Reducer implementation

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,37 @@
-export default 1;
+import produce from "immer";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    error: string;
+  };
+}
+
+const initialState: BundlesState = {};
+const reducer = produce(
+  (state: BundlesState = initialState, action: Action) => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START:
+        state[action.payload.cellId] = {
+          loading: true,
+          code: "",
+          error: "",
+        };
+        return state;
+      case ActionType.BUNDLE_COMPLETE:
+        state[action.payload.cellId] = {
+          loading: false,
+          ...action.payload.bundle,
+        };
+        return state;
+      default:
+        return state;
+    }
+  },
+  initialState
+);
+
+export default reducer;
