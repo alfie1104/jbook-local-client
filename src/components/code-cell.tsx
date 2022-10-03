3c812aeb73db43d68a1ffc94496c433faf2afbfc
peter_alfie <alfie1104@gmail.com>
2022-10-04 08:37:27 +0900

    feat(code-cell): adding a show function

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,7 +17,23 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [];
+
+    const cumulativeCode = [
+      `
+       const show = (value) => {
+        const root = document.querySelector('#root');
+        if(typeof value === 'object'){
+          if(value.$$typeof && value.props){
+            ReactDOM.render(value, root);
+          }else{
+            root.innerHTML = JSON.stringify(value);
+          }
+        }else{
+          root.innerHTML = value;
+        }
+       };
+      `,
+    ];
 
     for (let c of orderedCells) {
       //Accumulate codes from previous cell to current cell
