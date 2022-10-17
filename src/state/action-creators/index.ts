9e5bc677e96c63b0f1902627bfc3e75954acfb62
peter_alfie <alfie1104@gmail.com>
2022-10-18 08:22:10 +0900

    feat(action-creators): the fetch cells action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from "redux";
+import axios from "axios";
 import { ActionType } from "../action-types";
 import {
   DeleteCellAction,
@@ -10,7 +11,7 @@ import {
 } from "../actions";
 import bundle from "../../bundler";
 
-import { CellTypes } from "../cell";
+import { Cell, CellTypes } from "../cell";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -72,5 +73,26 @@ export const createBundle = (cellId: string, input: string) => {
         },
       },
     });
+  };
+};
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.FETCH_CELLS });
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get("/cells");
+      dispatch({
+        type: ActionType.FETCH_CELLS_COMPLETE,
+        payload: data,
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.FETCH_CELLS_ERROR,
+          payload: err.message,
+        });
+      }
+    }
   };
 };
